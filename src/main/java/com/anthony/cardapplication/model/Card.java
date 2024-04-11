ab447a13dbbe3f3453f543fd79295299cec14e35
Mohammed A <mohammed.alakashi2013@gmail.com>
2024-04-11 00:12:40 -0400

    Update Card.java

--- src/main/java/com/anthony/cardapplication/model/Card.java
+++ src/main/java/com/anthony/cardapplication/model/Card.java
@@ -28,45 +28,106 @@ public class Card implements Serializable {
     @Column(nullable = false)
     private String nationality;
 
-    @Lob
     @Column(nullable = false)
-    private String url;
+    private int year;
 
-    @Column(nullable = true)
+    @Column(nullable = false)
+    private String series;
+
+    @Column(nullable = false)
+    private int card_number;
+
+    @Column(nullable = false)
+    private boolean rookie;
+
+    @Column(nullable = false)
+    private String variant;
+
+    @Column(nullable = false)
     private String manufacturer;
 
-    public Card() {}
+    @Lob
+    @Column(nullable = true)
+    private String url;
 
-    public int getId() {
-        return Id;
+    @Lob
+    @Column(nullable = true)
+    private String description;
+
+
+    public Card() {
     }
 
-    public void setId(int id) {
-        this.Id = id;
+    public String getDescription() {
+        return description;
     }
 
-    public String getManufacturer() { return manufacturer; }
-
-    public void setManufacturer(String manufacturer) { this.manufacturer = manufacturer  ; }
-
-    public void setUrl(String url) { this.url = url; }
-
-    public String getUrl() { return url; }
-
-    public String getFirst_name() {
-        return first_name;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public String getUrl() {
+        return url;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public void setUrl(String url) {
+        this.url = url;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    public void setVariant(String variant) {
+        this.variant = variant;
+    }
+
+    public boolean isRookie() {
+        return rookie;
+    }
+
+    public void setRookie(boolean rookie) {
+        this.rookie = rookie;
+    }
+
+    public int getCard_number() {
+        return card_number;
+    }
+
+    public void setCard_number(int card_number) {
+        this.card_number = card_number;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
     }
 
     public String getClub() {
@@ -77,11 +138,27 @@ public class Card implements Serializable {
         this.club = club;
     }
 
-    public String getNationality() {
-        return nationality;
+    public String getLast_name() {
+        return last_name;
     }
 
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
     }
 }
